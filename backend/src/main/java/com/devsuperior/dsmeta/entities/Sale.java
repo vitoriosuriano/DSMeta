5e8625d3e532d3ab2af8b41e808457859c0f9159
unknown <vitoriosuriano.01@gmail.com>
2022-07-13 16:09:51 -0300

    Data Base

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,77 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private LocalDate date;
+	private Double amount;
+	private Integer deals;
+	private Integer visited;
+	
+public Sale() {
+	
+}
+
+public Long getId() {
+	return id;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+
+public String getSellerName() {
+	return sellerName;
+}
+
+public void setSellerName(String sellerName) {
+	this.sellerName = sellerName;
+}
+
+public LocalDate getDate() {
+	return date;
+}
+
+public void setDate(LocalDate date) {
+	this.date = date;
+}
+
+public Double getAmount() {
+	return amount;
+}
+
+public void setAmount(Double amount) {
+	this.amount = amount;
+}
+
+public Integer getDeals() {
+	return deals;
+}
+
+public void setDeals(Integer deals) {
+	this.deals = deals;
+}
+
+public Integer getVisited() {
+	return visited;
+}
+
+public void setVisited(Integer visited) {
+	this.visited = visited;
+}
+	
+}
